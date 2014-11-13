2f719925d08da5987133c9db05fcb3cd68bc34bf
joamag <joamag@gmail.com>
2014-11-13 12:55:47 +0000

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "omni_api",
-    version = "0.1.12",
+    version = "0.1.13",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Omni API",
